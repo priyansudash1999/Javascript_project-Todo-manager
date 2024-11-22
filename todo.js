c1651796ac6e5d9bc5bfef7d1ac13a49a0410114
1999priyansu <100997666+priyansudash1999@users.noreply.github.com>
2024-11-22 20:34:40 +0530

    adding date and time

--- todo.js
+++ todo.js
@@ -1,45 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
+    const dateInput = document.getElementById('date')
     const todo_input = document.getElementsByClassName('input')[0]
     const btn = document.getElementsByClassName('submit')[0]
     const todo_lists = document.getElementsByClassName('lists')[0]
 
     let tasks = JSON.parse(localStorage.getItem('tasks')) || []
-    console.log(typeof tasks)
-    tasks.forEach(task => {
-        displayTasksOnScreen(task)
+    tasks.forEach(t => {
+        showOnScreen(t)
     });
+
     btn.addEventListener('click', () => {
-    const task = todo_input.value.trim()
-    if(task === ""){
-        return
-    }
-    const todo_task = {
-        id: Date.now(),
-        text: task,
-        completed:false
-    }
-    tasks.push(todo_task)
-    console.log(tasks);  
-    saveTasks()
-    todo_input.value = ""
+        const task = todo_input.value.trim()
+        const date = dateInput.value.trim()
+        if(task === "" || date === ""){
+            return
+        }
+        const todo_task = {
+            id: Date.now(),
+            text: task,
+            date: date,
+            completed:false
+        }
+        tasks.push(todo_task) 
+        saveTasks()
+        showOnScreen(todo_task)
+        todo_input.value = ""
     })
 
-    function displayTasksOnScreen(task){
-        console.log(task);  
+    function showOnScreen(task){
         const li = document.createElement('li')
-        li.setAttribute('data-id', task.id)
-        li.innerHTML = `<span>${task.text}</span>
-                        <button class = 'delete-btn'>Delete</button>
-                        `
+        li.setAttribute('data_id', task.id)
+        li.innerHTML = `<span>${task.date} => ${task.text}</span>
+                        <button class="delete-btn">Delete</button>
+                       `
         todo_lists.appendChild(li)
-
-        li.addEventListener('click', (e)=>{
-            if(e.target.value === 'BUTTON') return
-            task.completed = !task.completed
-            li.classList.toggle("completed")
-        })
-
         li.classList.add('todo-item')
+
+        li.addEventListener('click', (e) => {
+            if(e.target.value === 'BUTTON'){
+                return
+            }
+            task.completed = !task.completed
+            li.classList.toggle('completed')
+        })
 
         li.querySelector('.delete-btn').addEventListener('click', (e) => {
             e.stopPropagation()
@@ -47,10 +50,12 @@ document.addEventListener("DOMContentLoaded", () => {
             li.remove()
             saveTasks()
         });
-        
     }
 
+
+
     function saveTasks(){
-        localStorage.setItem("tasks", JSON.stringify(tasks));
+        localStorage.setItem("tasks", JSON.stringify(tasks))
     }
+
 })
